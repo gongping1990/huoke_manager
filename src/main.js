fe0ef1b234b139c8faa2b6850edc94a2830e6584
hujinhui <394773007@qq.com>
2018-11-05 09:58:16 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import iView from "iview";
+import "iview/dist/styles/iview.css";
 
-Vue.config.productionTip = false
+Vue.use(iView);
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   render: h => h(App)
-})
+});
