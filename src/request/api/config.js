79e1a64b238fcbe896afa6d7de3565f42bca32a6
gongping1990 <wx_2d4ee0328b904aa789f1889f4721f6f3@git.code.tencent.com>
2019-01-11 14:47:53 +0800

    调整1

--- src/request/api/config.js
+++ src/request/api/config.js
@@ -3,22 +3,22 @@ import qs from 'qs'
 
 const config = {
   homeRecommendList(params) {
-    return axios.get('/config/list')
+    return axios.get('/config/homepage/list')
   },
   getHomeRecommendItem(params) {
-    return axios.get('/config/list',{params: params})
+    return axios.get('/config/homepage/list',{params: params})
   },
   // 新增
   addHomeRecommend(params) {
-    return axios.post('/config/add', params)
+    return axios.post('/config/homepage/add', params)
   },
   // 修改
   updateHomeRecommend(params) {
-    return axios.post('/config/update', params)
+    return axios.post('/config/homepage/update', params)
   },
   // 删除
   delHomeRecommend(params) {
-    return axios.post('/config/remove', qs.stringify(params))
+    return axios.post('/config/homepage/remove', qs.stringify(params))
   }
 }
 
