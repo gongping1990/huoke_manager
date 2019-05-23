13911087f582f696a149aeac2bfb23779606962f
gongping1990 <wx_2d4ee0328b904aa789f1889f4721f6f3@git.code.tencent.com>
2019-05-23 09:36:00 +0800

    调整16

--- src/view/marketingMangement/active/active.vue
+++ src/view/marketingMangement/active/active.vue
@@ -235,6 +235,10 @@
           return this.$Message.error('请选择体验课')
         } else if (this.addInfo.useScope && !this.courseListOne.length) {
           return this.$Message.error('请选择课程类型')
+        } else if (!this.addInfo.minMoney) {
+          return this.$Message.error('满减金额不能为0')
+        } else if (!this.addInfo.coverphoto) {
+          return this.$Message.error('请上传封面图')
         }
 
         this.addInfo.courseGoodsIdList = []
