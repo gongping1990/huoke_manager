efb2a1b9374caec7561065c4aaa4d3c5df363fcb
gongping1990 <wx_2d4ee0328b904aa789f1889f4721f6f3@git.code.tencent.com>
2019-07-17 14:35:51 +0800

    修改5
    # Conflicts:
    #	src/router/router.js

--- src/router/router.js
+++ src/router/router.js
@@ -5,6 +5,9 @@ import {tbzwRouter} from './routerSilder/tbzw'
 import {gswRouter} from './routerSilder/gsw'
 import {zlkRouter} from './routerSilder/zlk'
 import {ldRouter} from './routerSilder/ld'
+import {wzjhRouter} from './routerSilder/wzjh'
+import {ylxcxRouter} from './routerSilder/ylxcx'
+import {yhzxRouter} from './routerSilder/yhzx'
 // import err404 from '@/pages/404'
 //otherRouter
 // import home from '@/pages/home'
@@ -140,5 +143,8 @@ export const routers = [
   ...gswRouter,
   ...zlkRouter,
   ...ldRouter,
-  ...hkywRouter
+  ...hkywRouter,
+  ...wzjhRouter,
+  ...ylxcxRouter,
+  ...yhzxRouter
 ]
