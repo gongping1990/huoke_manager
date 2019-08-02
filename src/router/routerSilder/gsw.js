a28bdad5eb6ea4c3fff63054ee86ab60148f5509
gongping1990 <wx_2d4ee0328b904aa789f1889f4721f6f3@git.code.tencent.com>
2019-08-02 17:18:21 +0800

    修改31

--- src/router/routerSilder/gsw.js
+++ src/router/routerSilder/gsw.js
@@ -385,5 +385,22 @@ export const gswRouter =[
         component: () => import('@/view/gsw/contentManagement/contentList')
       }
     ]
+  },
+  {
+    path: '/teacherMan',
+    name: 'teacherMan',
+    title: '教师管理',
+    component: Main,
+    children: [
+      {
+        path: '/gsw_counselor',
+        name: 'gsw_counselor',
+        meta: [
+          {name: '教师管理'},
+          {name: '辅导教师'}
+        ],
+        component: () => import('@/view/gsw/teacherManagement/counselor')
+      }
+    ]
   }
 ]
